1ede2f1d075045a4d4a7c93890b14b23851fc150
DogukanTopallar <dtopallar1@gmail.com>
2022-08-28 17:31:59 +0300

    Session Added

---
+++ CoreDemo/BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator: AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı soyadı boş geçilemez!");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Yazar e-mail adresi boş geçilemez!");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Yazar şifresi boş geçilemez!");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("En az iki karakter giriniz!");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("En fazla elli karakter giriniz!");
+
+        }
+    }
+}
---
+++ CoreDemo/EntityLayer/Concrete/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        //ErişimBelirleyiciTürü DeğişkenTürü İsim {get set}
+        //prop & doubleTab
+        [Key]
+
+        public int CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string CategoryDescription { get; set; }
+
+        public bool CategoryStatus { get; set; }
+
+
+        public List<Blog> Blogs { get; set; }
+
+
+    }
+}
