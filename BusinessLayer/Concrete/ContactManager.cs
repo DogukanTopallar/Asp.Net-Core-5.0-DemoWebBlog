41ee2a64aef7b9ab865700d370bad7f1bf940a5e
DogukanTopallar <dtopallar1@gmail.com>
2022-08-28 16:15:16 +0300

    Updates on Blogs pages and Writers Panel

---
+++ BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ContactManager: IContactService
+    {
+        IContactDal _cantactdal;
+
+        public ContactManager(IContactDal cantactdal)
+        {
+            _cantactdal = cantactdal;
+        }
+
+        public void ContactAdd(Contact contact)
+        {
+            _cantactdal.Insert(contact);
+        }
+    }
+}
