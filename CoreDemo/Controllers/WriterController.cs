6bd5af407db87f23490e1c7eb1761ee743a1d3d5
DogukanTopallar <dtopallar1@gmail.com>
2022-08-28 16:54:33 +0300

    Authentication on Writers Panels

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -7,10 +7,20 @@ using System.Threading.Tasks;
 
 namespace CoreDemo.Controllers
 {
+    [Authorize]
     public class WriterController : Controller
     {
-        [Authorize]
+        
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+        public IActionResult WriterMail()
         {
             return View();
         }
