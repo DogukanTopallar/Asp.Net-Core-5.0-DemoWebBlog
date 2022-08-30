5edbf4f795a00a8b470fea970d626978f43d711e
DogukanTopallar <dtopallar1@gmail.com>
2022-08-30 13:12:31 +0300

    Activated Footer
    The last posts on the footer were dynamited.

--- CoreDemo/Controllers/ContactController.cs
+++ CoreDemo/Controllers/ContactController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace CoreDemo.Controllers
 {
-    
+    [AllowAnonymous]
     public class ContactController : Controller
     {
         ContactManager cm = new ContactManager(new EfContactRepository());
